fcadea038e0a542744f1de0b68e5b8cb1562b912
Kaiqiang Duan <moonshile@foxmail.com>
2015-03-14 18:16:43 +0800

    remove useless files

--- src/goal/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from goal.models import Goal
-
-admin.site.register(Goal)
-
--- src/static/js/box.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-    $(".optional-header").click(function(){
-        $(".optional-header .caret").css("transform", $(".optional-body").is(":hidden") ? "rotate(0)" : "rotate(-90deg)");
-        $(".optional-body").slideToggle(500);
-    })
-});
