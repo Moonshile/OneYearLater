6f494e2b9467bbfaeb4d40c2a65f862677e6e686
Kaiqiang Duan <moonshile@foxmail.com>
2015-03-25 15:51:28 +0800

    basic sign up

--- src/account/forms.py
+++ src/account/forms.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from django import forms
 from models import InvitationCode
 
@@ -6,7 +7,7 @@ import re
 max_len_msg = u'不长于%d个字符'
 min_len_msg = u'不短于%d个字符'
 
-class SignUpForm(forms.Form):
+class SignupForm(forms.Form):
     username = forms.CharField(max_length=10, min_length=3, error_messages={
         'max_length': max_len_msg % 10,
         'min_length': min_len_msg % 3,
@@ -23,35 +24,35 @@ class SignUpForm(forms.Form):
     password2 = forms.CharField(error_messages={
         'invalid': u'与第一次输入一致',
     })
-    invitation_code = forms.CharField(error_messages={
+    invitation_code = forms.CharField(required=False, error_messages={
         'invalid': u'是字母或者数字',
     })
 
     def clean_username(self):
-        pattern = re.compile(u'^[a-zA-Z0-9_\u4e00-\u9fa5]+$')
+        pattern = re.compile(r'^[a-zA-Z0-9_\u4e00-\u9fa5]+$')
         username = self.cleaned_data['username']
         if not pattern.match(username):
-            raise forms.ValidationError(_(u'是字母、数字或者汉字'), code='username')
+            raise forms.ValidationError((u'是字母、数字或者汉字'), code='content')
         return username
 
     def clean_password(self):
-        pattern = re.compile(u'^[\x21-\x7e]+$')
+        pattern = re.compile(r'^[\x21-\x7e]+$')
         password = self.cleaned_data['password']
         if not pattern.match(password):
-            raise forms.ValidationError(_(u'是半角字母、数字或者标点符号'), code='password')
+            raise forms.ValidationError((u'是半角字母、数字或者标点符号'), code='content')
         return password
 
     def clean_password2(self):
         password2 = self.cleaned_data['password2']
         password = self.data['password']
         if password2 != password:
-            raise forms.ValidationError(_(u'与第一次输入一致'), code='password2')
+            raise forms.ValidationError((u'与第一次输入一致'), code='same')
         return password2
 
     def clean_invitation_code(self):
         invitation_code = self.cleaned_data['invitation_code']
         if InvitationCode.objects.filter(code=invitation_code).count() == 0:
-            raise forms.ValidationError(_(u'邀请码错误'), code='invitation_code')
+            raise forms.ValidationError((u'邀请码错误'), code='exists')
         raise invitation_code
 
 class SigninForm(forms.Form):
@@ -65,17 +66,17 @@ class SigninForm(forms.Form):
     })
 
     def clean_username(self):
-        pattern = re.compile(u'^[a-zA-Z0-9_\u4e00-\u9fa5]+$')
+        pattern = re.compile(r'^[a-zA-Z0-9_\u4e00-\u9fa5]+$')
         email = forms.EmailField()
         username = self.cleaned_data['username']
         if not pattern.match(username) or not email.clean(username):
-            raise forms.ValidationError(_(u'是字母、数字或者汉字，或者你的邮箱'), code='username')
+            raise forms.ValidationError((u'是字母、数字或者汉字，或者你的邮箱'), code='content')
         return username
 
     def clean_password(self):
-        pattern = re.compile(u'^[\x21-\x7e]+$')
+        pattern = re.compile(r'^[\x21-\x7e]+$')
         password = self.cleaned_data['password']
         if not pattern.match(password):
-            raise forms.ValidationError(_(u'是半角字母、数字或者标点符号'), code='password')
+            raise forms.ValidationError((u'是半角字母、数字或者标点符号'), code='content')
         return password
 
--- src/account/views.py
+++ src/account/views.py
@@ -1,11 +1,15 @@
-
+#coding=utf-8
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
+
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.http import require_http_methods
+
 from django.contrib.auth.models import User
 
-from account.models import Account
+from models import Account
+from forms import SignupForm, SigninForm
 
 def account(request):
     return render_to_response('bowl.html')
@@ -22,7 +26,21 @@ def signin(request):
 
 @ensure_csrf_cookie
 def signup(request):
-    
-    return render_to_response('signup.html')
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(
+                username = cd['username'],
+                password = cd['password'],
+                email = cd['email'],
+            )
+            user.save()
+            account = Account(owner=user)
+            account.save()
+            return render_to_response('signup.html', RequestContext(request))
+        return render_to_response('signup.html', RequestContext(request, {'err': form.errors}))
+    else:
+        return render_to_response('signup.html', RequestContext(request))
 
 
